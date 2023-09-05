282137dfaa9d6d7ef57823b5b52a999bb583e0e3
JyothikaNambiar <82314985+JyothikaNambiar@users.noreply.github.com>
2023-09-05 06:35:05 +0530

    Update and rename Chatbot1.yaml to Chatbot2/Chatbot2.py

---
+++ Chatbot2/Chatbot2.py
@@ -0,0 +1,10 @@
+from nemoguardrails.rails import LLMRails, RailsConfig
+import openai
+import os
+openai.api_key = ('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')
+os.environ['OPENAI_API_KEY'] = ('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')
+config = RailsConfig.from_path("Chatbot2.yaml")
+app = LLMRails(config, verbose=False)
+history = [{"role": "user", "content": "When did Neil Armstrong walk on the moon with Jyothika?"}]
+bot_message = await app.generate_async(messages=history)
+print(bot_message['content'])
