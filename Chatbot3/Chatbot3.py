a7ee3e3a5eef2c5252e21c265256380022fb7fe9
JyothikaNambiar <82314985+JyothikaNambiar@users.noreply.github.com>
2023-09-05 06:42:47 +0530

    Update and rename chatbot1.py to Chatbot3/Chatbot3.py

--- Chatbot3/Chatbot3.py
+++ Chatbot3/Chatbot3.py
@@ -3,7 +3,7 @@ import openai
 import os
 openai.api_key = ('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')
 os.environ['OPENAI_API_KEY'] = ('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')
-config = RailsConfig.from_path("Chatbot1.yaml")
+config = RailsConfig.from_path("Chatbot3.yaml")
 app = LLMRails(config, verbose=False)
 history = [{"role": "user", "content": "When did Neil Armstrong walk on the moon with Jyothika?"}]
 bot_message = await app.generate_async(messages=history)
