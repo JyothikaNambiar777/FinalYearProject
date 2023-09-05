510fd7195ac48f0823e50c9f2c2dfacc1e262e9d
JyothikaNambiar <82314985+JyothikaNambiar@users.noreply.github.com>
2023-09-05 06:29:03 +0530

    Update chatbot1.py

--- chatbot1.py
+++ chatbot1.py
@@ -3,7 +3,7 @@ import openai
 import os
 openai.api_key = ('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')
 os.environ['OPENAI_API_KEY'] = ('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')
-config = RailsConfig.from_path("/Users/Jyothika/Documents/FinalYear/config")
+config = RailsConfig.from_path("Chatbot1.yaml")
 app = LLMRails(config, verbose=False)
 history = [{"role": "user", "content": "When did Neil Armstrong walk on the moon with Jyothika?"}]
 bot_message = await app.generate_async(messages=history)
